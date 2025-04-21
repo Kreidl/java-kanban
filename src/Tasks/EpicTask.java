ee082b1dc83bb793b875d40b28ae60d321c4a1f8
Карина <karinkademchenko@yandex.ru>
2025-04-21 21:11:32 +0300

    Update Class EpicTask

--- src/Tasks/EpicTask.java
+++ src/Tasks/EpicTask.java
@@ -10,26 +10,6 @@ public class EpicTask extends Task {
         this.setTaskStatus(TaskStatus.NEW);
     }
 
-    public EpicTask(String name, String description, TaskStatus taskStatus) {
-        super(name, description, taskStatus);
-    }
-
-    public void addNewSubtask (Subtask subtask) {
-        subtasks.add(subtask);
-    }
-
-    public void updateSubtask (Subtask thisSubtask, Subtask updatedSubtask) {
-        for (Subtask subtask : subtasks) {
-            if (subtask.equals(thisSubtask)) {
-                subtask.setName(updatedSubtask.getName());
-                subtask.setDescription(updatedSubtask.getDescription());
-                subtask.setTaskStatus(updatedSubtask.getTaskStatus());
-                int indexSubtask = subtasks.indexOf(subtask);
-                subtasks.set(indexSubtask, subtask);
-            }
-        }
-    }
-
     public ArrayList<Subtask> getSubtasks() {
         return subtasks;
     }
