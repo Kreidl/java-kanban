ce2cd3da8bca822952649d1c9fe18597bf276121
Карина <karinkademchenko@yandex.ru>
2025-05-02 20:04:24 +0300

    Add interface HistoryManager

---
+++ src/Manager/HistoryManager.java
@@ -0,0 +1,12 @@
+package Manager;
+
+import Tasks.Task;
+
+import java.util.ArrayList;
+
+public interface HistoryManager {
+
+    public void add(Task task);
+
+    public ArrayList<Task> getHistory();
+}
