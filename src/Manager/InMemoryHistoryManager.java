e3c8d6bf63605f83f8408e4b4abd8ea147974580
Карина <karinkademchenko@yandex.ru>
2025-05-02 20:05:08 +0300

    Add class InMemoryHistoryManager

---
+++ src/Manager/InMemoryHistoryManager.java
@@ -0,0 +1,25 @@
+package Manager;
+
+import Tasks.Task;
+
+import java.util.ArrayList;
+
+public class InMemoryHistoryManager implements HistoryManager {
+    private final ArrayList<Task> historyOfTasks = new ArrayList<>();
+
+
+    @Override
+    public ArrayList<Task> getHistory() {
+        return historyOfTasks;
+    }
+
+    @Override
+    public void add(Task task) {
+        if (historyOfTasks.size() == 10) {
+            historyOfTasks.removeFirst();
+            historyOfTasks.add(task);
+        } else if (historyOfTasks.size() < 10) {
+            historyOfTasks.add(task);
+        }
+    }
+}
