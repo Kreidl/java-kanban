e7e72b66b6765340acd81c9f1b03aab313761de7
Карина <karinkademchenko@yandex.ru>
2025-05-03 16:51:27 +0300

    Add SubtaskTest

---
+++ test/Tasks/SubtaskTest.java
@@ -0,0 +1,27 @@
+package Tasks;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SubtaskTest {
+
+    Subtask subtask;
+
+    @BeforeEach
+    void createNewSubtask() {
+        subtask = new Subtask("Подзадача 1.1", "Описание подзадачи 1.1", TaskStatus.NEW, 1);
+    }
+
+    @Test
+    void getEpicId() {
+        assertEquals(1, subtask.getEpicId(), "Id эпика в подзадаче не соответствует ожидаемому");
+    }
+
+    @Test
+    void setEpicId() {
+        subtask.setEpicId(2);
+        assertEquals(2, subtask.getEpicId(), "Id эпика в подзадаче после изменения не соответствует ожидаемому");
+    }
+}
