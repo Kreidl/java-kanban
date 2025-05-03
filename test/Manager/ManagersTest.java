f2f9113e90c30077959b72dd3cc70167a59b6f6a
Карина <karinkademchenko@yandex.ru>
2025-05-03 16:51:43 +0300

    Add ManagersTest

---
+++ test/Manager/ManagersTest.java
@@ -0,0 +1,20 @@
+package Manager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class ManagersTest {
+
+    @Test
+    void getDefault() {
+        TaskManager taskManager = Managers.getDefault();
+        assertNotNull(taskManager, "Дефолтный менеджер не найден");
+    }
+
+    @Test
+    void getDefaultHistory() {
+        HistoryManager historyManager = Managers.getDefaultHistory();
+        assertNotNull(historyManager, "Дефолтная история просмотров для менеджера не найдена");
+    }
+}
