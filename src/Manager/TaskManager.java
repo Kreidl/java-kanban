219c860044821352d6e2a7edb7b5d1abc9e70a58
Карина <karinkademchenko@yandex.ru>
2025-05-04 12:50:04 +0300

    Update TaskManager

--- src/Manager/TaskManager.java
+++ src/Manager/TaskManager.java
@@ -7,43 +7,43 @@ import Tasks.Task;
 import java.util.ArrayList;
 
 public interface TaskManager {
-    public void addTask(Task task);
+    void addTask(Task task);
 
-    public void addEpicTask(EpicTask epicTask);
+    void addEpicTask(EpicTask epicTask);
 
-    public void addSubtask(Subtask subtask);
+    void addSubtask(Subtask subtask);
 
-    public void updateTask(Task thisTask, Task updatedTask);
+    void updateTask(Task thisTask, Task updatedTask);
 
-    public void updateEpicTask(EpicTask thisEpicTask, EpicTask updatedEpicTask);
+    void updateEpicTask(EpicTask thisEpicTask, EpicTask updatedEpicTask);
 
-    public void updateSubtask(Subtask thisSubtask, Subtask updatedSubtask);
+    void updateSubtask(Subtask thisSubtask, Subtask updatedSubtask);
 
-    public void deleteTaskById(Task task);
+    void deleteTaskById(Task task);
 
-    public void deleteEpicTaskById(EpicTask epicTask);
+    void deleteEpicTaskById(EpicTask epicTask);
 
-    public void deleteSubtaskById(Subtask subtask);
+    void deleteSubtaskById(Subtask subtask);
 
-    public Task getTaskById(int taskId);
+    Task getTaskById(int taskId);
 
-    public EpicTask getEpicTaskById(int taskId);
+    EpicTask getEpicTaskById(int taskId);
 
-    public Subtask getSubtaskById(int taskId);
+    Subtask getSubtaskById(int taskId);
 
-    public ArrayList<Task> getAllTasks();
+    ArrayList<Task> getAllTasks();
 
-    public ArrayList<EpicTask> getAllEpicTasks();
+    ArrayList<EpicTask> getAllEpicTasks();
 
-    public ArrayList<Subtask> getAllSubtasks();
+    ArrayList<Subtask> getAllSubtasks();
 
-    public void deleteAllTasks();
+    void deleteAllTasks();
 
-    public void deleteAllEpicTasks();
+    void deleteAllEpicTasks();
 
-    public void deleteAllSubtasks();
+    void deleteAllSubtasks();
 
-    public ArrayList<Subtask> getEpicSubtasks(int epicId);
+    ArrayList<Subtask> getEpicSubtasks(int epicId);
 
-    public ArrayList<Task> getHistory();
+    ArrayList<Task> getHistory();
 }
